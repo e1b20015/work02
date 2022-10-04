993f4145691c037974be93f19db502ca8d41ca80
e1b20043 <e1b20043@oit.ac.jp>
2022-10-04 14:23:09 +0900

    first branch

---
+++ src/main/java/oit/is/team08/kaizi/work02/Work02Application.java
@@ -0,0 +1,13 @@
+package oit.is.team08.kaizi.work02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work02Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team08/kaizi/work02/Work02ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team08.kaizi.work02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
